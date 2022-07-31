466c3e57babb6798c2a9abd3c28e6559340438a7
therealcyberlord <30872286+therealcyberlord@users.noreply.github.com>
2022-07-31 13:14:59 -0400

    it
    slight change

--- DCGAN.py
+++ DCGAN.py
@@ -2,22 +2,27 @@ from torch import nn
 import torch
 import torch.nn.functional as F
     
+# Generator Code
+
+ngf = 64
+num_channels = 3
+
 class Generator(nn.Module):
     def __init__(self, latent_size):
         super(Generator, self).__init__()
 
         self.latent_size = latent_size
         self.conv1 = nn.ConvTranspose2d(
-            self.latent_size, 64*8, 4, 1, 0, bias=False)
-        self.bn1 = nn.BatchNorm2d(64*8)
-        self.conv2 = nn.ConvTranspose2d(64*8, 64*4, 4, 2, 1, bias=False)
-        self.bn2 = nn.BatchNorm2d(64*4)
-        self.conv3 = nn.ConvTranspose2d(64*4, 64*2, 4, 2, 1, bias=False)
-        self.bn3 = nn.BatchNorm2d(64*2)
-        self.conv4 = nn.ConvTranspose2d(64*2, 64, 4, 2, 1, bias=False)
-        self.bn4 = nn.BatchNorm2d(64)
+            self.latent_size, ngf*8, 4, 1, 0, bias=False)
+        self.bn1 = nn.BatchNorm2d(ngf*8)
+        self.conv2 = nn.ConvTranspose2d(ngf*8, ngf*4, 4, 2, 1, bias=False)
+        self.bn2 = nn.BatchNorm2d(ngf*4)
+        self.conv3 = nn.ConvTranspose2d(ngf*4, ngf*2, 4, 2, 1, bias=False)
+        self.bn3 = nn.BatchNorm2d(ngf*2)
+        self.conv4 = nn.ConvTranspose2d(ngf*2, ngf, 4, 2, 1, bias=False)
+        self.bn4 = nn.BatchNorm2d(ngf)
 
-        self.conv5 = nn.ConvTranspose2d(64, 3, 4, 2, 1, bias=False)
+        self.conv5 = nn.ConvTranspose2d(ngf, num_channels, 4, 2, 1, bias=False)
 
     def forward(self, x):
         x = F.relu(self.bn1(self.conv1(x)), inplace=True)
@@ -25,29 +30,3 @@ class Generator(nn.Module):
         x = F.relu(self.bn3(self.conv3(x)), inplace=True)
         x = F.relu(self.bn4(self.conv4(x)), inplace=True)
         return torch.tanh(self.conv5(x))
-
-# code for the discriminator, which classifies input as real or fake
-
-
-class Discriminator(nn.Module):
-    def __init__(self):
-        super(Discriminator, self).__init__()
-
-        self.conv1 = nn.Conv2d(3, 64, 4, 2, 1, bias=False)
-        self.conv2 = nn.Conv2d(64, 64*2, 4, 2, 1, bias=False)
-        self.bn1 = nn.BatchNorm2d(64*2)
-        self.conv3 = nn.Conv2d(64*2, 64*4, 4, 2, 1, bias=False)
-        self.bn2 = nn.BatchNorm2d(64*4)
-        self.conv4 = nn.Conv2d(64*4, 64*8, 4, 2, 1, bias=False)
-        self.bn3 = nn.BatchNorm2d(64*8)
-        self.conv5 = nn.Conv2d(64*8, 1, 4, 1, 0, bias=False)
-
-    def forward(self, x):
-        x = F.leaky_relu(self.conv1(x), negative_slope=0.2, inplace=True)
-        x = F.leaky_relu(self.bn1(self.conv2(x)),
-                         negative_slope=0.2, inplace=True)
-        x = F.leaky_relu(self.bn2(self.conv3(x)),
-                         negative_slope=0.2, inplace=True)
-        x = F.leaky_relu(self.bn3(self.conv4(x)),
-                         negative_slope=0.2, inplace=True)
-        return torch.sigmoid(self.conv5(x))

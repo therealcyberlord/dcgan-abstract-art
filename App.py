f24ecb2b829463c19d00e407874b7e21f24bed6d
therealcyberlord <30872286+therealcyberlord@users.noreply.github.com>
2022-08-01 16:07:14 -0400

    column layout

--- App.py
+++ App.py
@@ -12,17 +12,14 @@ if torch.cuda.is_available():
     device = torch.device("cuda")
 
 latent_size = 100
-display_width = 450
 checkpoint_path = "Checkpoints/150epochs.chkpt"
 
 st.title("Generating Abstract Art")
-st.text("start generating (left side bar)")
-st.text("Made by Xingyu B.")
 
 st.sidebar.subheader("Configurations")
-seed = st.sidebar.slider('Seed', -10000, 10000, 0)
+seed = st.sidebar.slider('Seed', -100000, 100000, 0)
 
-num_images = st.sidebar.slider('Number of Images', 1, 10, 1)
+num_images = st.sidebar.slider('Number of Images', 1, 8, 1)
 
 use_srgan = st.sidebar.selectbox(
     'Apply image enhancement',
@@ -30,7 +27,7 @@ use_srgan = st.sidebar.selectbox(
 )
 
 generate = st.sidebar.button("Generate")
-
+st.write("Get started using the left side bar :sunglasses:")
 
 # caching the expensive model loading 
 
@@ -59,7 +56,6 @@ if generate:
     # use srgan for super resolution
     if use_srgan == "Yes":
         # restore to the checkpoint
-        st.write("Using DCGAN then ESRGAN upscale...")
         esrgan_generator = SRGAN.GeneratorRRDB(channels=3, filters=64, num_res_blocks=23).to(device)
         esrgan_checkpoint = load_esrgan()
         esrgan_generator.load_state_dict(esrgan_checkpoint)
@@ -69,17 +65,21 @@ if generate:
             enhanced_fakes = esrgan_generator(fakes).detach().cpu()
         color_match = color_histogram_mapping(enhanced_fakes, fakes.cpu())
 
+        cols = st.columns(num_images)
         for i in range(len(color_match)):
             # denormalize and permute to correct color channel
-            st.image(denormalize_images(color_match[i]).permute(1, 2, 0).numpy(), width=display_width)
+            cols[i].image(denormalize_images(color_match[i]).permute(1, 2, 0).numpy(), use_column_width=True)
 
 
     # default setting -> vanilla dcgan generation
     if use_srgan == "No":
         fakes = fakes.cpu()
-        st.write("Using DCGAN Model...")
+
+        cols = st.columns(num_images)
         for i in range(len(fakes)):
-            st.image(denormalize_images(fakes[i]).permute(1, 2, 0).numpy(), width=display_width)
+            cols[i].image(denormalize_images(fakes[i]).permute(1, 2, 0).numpy(), use_column_width=True)
+
+
 
 
 
